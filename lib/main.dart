2c13612828d219ce6f2f0d6bd779aa5986406d41
hardiknatani <hardiknatani202@gmail.com>
2022-11-08 11:07:30 +0530

    lib adaptive_navbar

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:adaptive_navbar/adaptive_navbar.dart';
+import 'package:flutter/material.dart';
+
+/// Entry point of the application
+void main() {
+  runApp(const MyApp());
+}
+
+/// Base structure of the app.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Adaptive NavBar',
+      home: HomePage(),
+    );
+  }
+}
+
+/// HomePage of the app.
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    /// Screen Width of the device.
+    final sw = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      drawer: sw < 800 ? Drawer(child: Text('drawer')) : null,
+      appBar: AdaptiveNavBar(
+        screenWidth: sw,
+        title: const Text("Adaptive NavBar"),
+        navBarItems: [
+          NavBarItem(
+            text: "Home",
+            onTap: () {
+              Navigator.pushNamed(context, "routeName");
+            },
+          ),
+          NavBarItem(
+            text: "About Us",
+            onTap: () {
+              Navigator.pushNamed(context, "routeName");
+            },
+          ),
+          NavBarItem(
+            text: "About Us",
+            onTap: () {
+              Navigator.pushNamed(context, "routeName");
+            },
+          ),
+          NavBarItem(
+            text: "About Us",
+            onTap: () {
+              Navigator.pushNamed(context, "routeName");
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'This package, "ADAPTIVE NAVBAR", was developed by Mouli Bheemaneti.',
+        ),
+      ),
+    );
+  }
+}
